df3837b1b6f7486c01d277388648d5d0e30c4c32
Shamsher <ansarisam47@gmail.com>
2015-08-05 22:25:27 +0530

    Add comments to understand first program

--- hello-time.go
+++ hello-time.go
@@ -1,10 +1,24 @@
+/**
+ * Package Declaration
+ * Every Go programe must start with Package declaration
+ * Packages are Go's way of organizing and re-using code
+ */
 package main
 
+/**
+ * Import keyword is used to include other packages
+ * fmt(format) package is used for formatting
+ */
 import (
   "fmt"
   "time"
 )
 
+/**
+ * func is keyword for writing function
+ * Println(Print Line)
+ * No semi colon at the end of line
+ */
 func main() {
   fmt.Println("My first awesome go programe")
 
