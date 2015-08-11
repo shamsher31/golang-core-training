8aa812830ca08a9582e2a67db78d8d158d82eba5
Shamsher <ansarisam47@gmail.com>
2015-08-11 22:34:47 +0530

    Add go pointer example

---
+++ go-pointer.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+ * Pointer points to memory location
+ * '*' is pointer in go, it is also
+ * used to refrence value stored in poniter
+ * '&' is used to refrence pointer value
+ */
+func zipcode(p *int) {
+	*p = 401203
+}
+
+func main() {
+
+	pinCode := 411001
+
+	zipcode(&pinCode)
+
+	fmt.Println(pinCode)
+
+}
