0b749041b62f86eee2c386cd77dddf151987be38
Shamsher <ansarisam47@gmail.com>
2015-08-15 17:10:50 +0530

    Add ping pong channel example

---
+++ go-channel.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// '<-'(left arrow) operator is used
+// to send and recive msg between channel
+// c <- means send msg
+// msg := <- c means recive msg
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "Ping"
+	}
+}
+
+func ponger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "Pong"
+	}
+}
+
+func printer(c chan string) {
+	for {
+		// print msg recived on channel
+		fmt.Println(<-c)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+// Channel provides communication
+// between goroutines and sync
+// execution between them
+func main() {
+
+	// 'chan' is used to create channels
+	var c chan string = make(chan string)
+
+	// call goroutine
+	go pinger(c)
+	go ponger(c)
+	go printer(c)
+
+	var input string
+	fmt.Scanln(&input)
+
+}
