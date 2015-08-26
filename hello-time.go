94dcebd980d12152823a7c3ab804e6d1ec34f470
Shamsher Ansari <ansari.shamsher31@gmail.com>
2015-08-26 19:38:40 +0530

    Change comment style
    Change comment style to be readable by godoc

--- hello-time.go
+++ hello-time.go
@@ -1,24 +1,23 @@
-/**
- * Package Declaration
- * Every Go programe must start with Package declaration
- * Packages are Go's way of organizing and re-using code
- */
+
+ // Package Declaration
+ // Every Go programe must start with Package declaration
+ // Packages are Go's way of organizing and re-using code
+ 
 package main
 
-/**
- * Import keyword is used to include other packages
- * fmt(format) package is used for formatting
- */
+ // Import keyword is used to include other packages
+ // fmt(format) package is used for formatting
+ 
 import (
 	"fmt"
 	"time"
 )
 
-/**
- * func is keyword for writing function
- * Println(Print Line)
- * No semi colon at the end of line
- */
+
+ // func is keyword for writing function
+ // Println(Print Line)
+ // No semi colon at the end of line
+
 func main() {
 	fmt.Println("My first awesome go programe")
 
