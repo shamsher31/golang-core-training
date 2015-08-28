5e88440b8d26130025496b2c236dfc039807290d
Shamsher <ansarisam47@gmail.com>
2015-08-29 00:18:27 +0530

    Add explaination for import types

--- hello-time.go
+++ hello-time.go
@@ -6,6 +6,11 @@ package main
 
 // Import keyword is used to include other packages
 // fmt(format) package is used for formatting
+// You can import packages in 3 way
+// using '.' will omit package name so you can write Println instead of fmt.Println
+// using 'aliase' so you can write f "fmt" and write f.Println
+// using '_' like _ "fmt" so you can use only init() from package
+// and you can not use methods from package
 
 import (
 	"fmt"
