8e2a5abc91c77d21a4fd6886df418af626260dae
Shamsher <ansarisam47@gmail.com>
2015-11-01 16:46:48 +0530

    Fixe gofmt issue using GoSublime package by adding GOPATH and GOROOT in GoSublime default config env

--- go-web/web-server.go
+++ go-web/web-server.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hi this is first web server in %s", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi this is first web server in %s", r.URL.Path[1:])
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":6060", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":6060", nil)
 }
-
--- go-web/wiki.go
+++ go-web/wiki.go
@@ -2,16 +2,16 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
 )
 
 // Page struct define title and body
 // Body is a bytes slice bcoz ie expected by
 // io library we are using
 type Page struct {
-  Title string
-  Body []byte
+	Title string
+	Body  []byte
 }
 
 // save method takes pointer to page as a reciver
@@ -19,8 +19,8 @@ type Page struct {
 // error is a return type of WriteFile
 // 0600 allows read write permission to file
 func (p *Page) save() error {
-  filename := p.Title + ".txt"
-  return ioutil.WriteFile(filename, p.Body, 0600)
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 // loadPage takes title as a param
@@ -29,19 +29,19 @@ func (p *Page) save() error {
 // ReadFile reads the content of the file
 // &page returns page pointer
 func loadPage(title string) (*Page, error) {
-  filename := title + ".txt"
-  body, err := ioutil.ReadFile(filename)
-  
-  if err != nil {
-    return nil, err
-  }
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
 
-  return &Page{Title: title, Body: body}, nil
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{Title: title, Body: body}, nil
 }
 
 func main() {
-  p1 := &Page{Title: "FirstPage", Body: []byte("Body content goes here")}
-  p1.save()
-  p2 , _ := loadPage("FirstPage")
-  fmt.Println(string(p2.Body))
+	p1 := &Page{Title: "FirstPage", Body: []byte("Body content goes here")}
+	p1.save()
+	p2, _ := loadPage("FirstPage")
+	fmt.Println(string(p2.Body))
 }
