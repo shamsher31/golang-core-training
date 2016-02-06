dd9072a5d3c1d780a6f8f91afa5619c7e16afaea
Shamsher <ansarisam47@gmail.com>
2016-02-06 22:05:39 +0530

    Add log for command line to see server running

--- go-web/web-server.go
+++ go-web/web-server.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi this is first web server in %s", r.URL.Path[1:])
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi this is first web server in %s", r.URL.Path[:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", root)
+	log.Printf("Server is running on http://localhost:6060")
 	http.ListenAndServe(":6060", nil)
 }
