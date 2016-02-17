5668b7ca598638c148a1a104289c1001cb09a4bb
Shamsher <ansarisam47@gmail.com>
2016-02-17 10:27:17 +0530

    Add advance slice operations

---
+++ go-basics/go-advance-slice.go
@@ -0,0 +1,64 @@
+// Ref: http://blog.golang.org/slices
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// define path as a byte slice
+type path []byte
+
+func main() {
+
+	var buffer [256]byte
+
+	// Add 10 to last element of array
+	buffer[255] = 10
+
+	fmt.Println("Array buffer :", buffer)
+
+	// create slice from array [start:end]
+	slice := buffer[100:150]
+
+	AddOneToEachElement(slice)
+	fmt.Println("Part of the array : ", slice)
+
+	SubstractOneFromLength(slice)
+	fmt.Println("After substraction : ", slice)
+
+	pathName := path("/use/local/bin")
+
+	fmt.Println("Path : ", string(pathName))
+
+	pathName.TruncateAtFinalSlash()
+	fmt.Println("Modified path : ", string(pathName))
+
+}
+
+func AddOneToEachElement(slice []byte) {
+	for i := range slice {
+		slice[i] += 1
+	}
+}
+
+func SubstractOneFromLength(slice []byte) []byte {
+	// remove last elem of the slice
+	slice = slice[0 : len(slice)-1]
+	return slice
+}
+
+func (p *path) TruncateAtFinalSlash() {
+
+	// Verify if the byte has a / in the end
+	// and return its index, if not exist return -1
+	i := bytes.LastIndex(*p, []byte("/"))
+	fmt.Println("index", i)
+
+	if i > 0 {
+		// remmove value from last index
+		*p = (*p)[0:i]
+	}
+
+}
