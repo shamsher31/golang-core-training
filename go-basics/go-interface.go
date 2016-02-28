7385605a4a0188f7a90b1d43906c205a75fe1529
Shamsher <ansarisam47@gmail.com>
2016-02-28 12:02:52 +0530

    Update interface example

--- go-basics/go-interface.go
+++ go-basics/go-interface.go
@@ -18,10 +18,41 @@ func (c Cat) Speak() string {
 	return "Meow!!"
 }
 
+type Student interface {
+	getFullName() string
+	getClass() string
+	getRollNo() int
+}
+
+type student struct {
+	FullName string
+	Class    string
+	RollNo   int
+}
+
+func (s *student) getFullName() string {
+	return s.FullName
+}
+
+func (s *student) getClass() string {
+	return s.Class
+}
+
+func (s *student) getRollNo() int {
+	return s.RollNo
+}
+
 func main() {
 	animals := []Animal{Dog{}, Cat{}}
 
 	for _, animal := range animals {
 		fmt.Println(animal.Speak())
 	}
+
+	std := student{"Shamsher Ansari", "BE", 24}
+	fmt.Println("<<<< Student Details >>>>")
+	fmt.Println("Full Name : ", std.getFullName())
+	fmt.Println("Class : ", std.getClass())
+	fmt.Println("Roll No. : ", std.getRollNo())
+
 }
